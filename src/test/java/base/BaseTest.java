adbf215e6fab4a97e572d75f4fc5d5df55f2431e
Rafael Goncalves Rodrigues <rafael.rodrigues@cwi.com.br>
2018-12-20 16:44:31 -0200

    wip

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,11 +6,6 @@ import org.junit.AfterClass;
 
 public class BaseTest {
 
-    @After
-    public void tearDown() {
-        Chrome.getDriver().get("https://www.google.com");
-    }
-
     @AfterClass
     public static void clean() {
         Chrome.getDriver().quit();
