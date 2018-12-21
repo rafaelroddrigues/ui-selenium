ce734138d3a39a5518b75fdd71ac4ec3ac3dd728
Rafael Goncalves Rodrigues <rafael.rodrigues@cwi.com.br>
2018-12-21 12:22:33 -0200

    wip

--- src/main/java/page/search/HomePage.java
+++ src/main/java/page/search/HomePage.java
@@ -6,8 +6,24 @@ import page.Page;
 
 public class HomePage extends Page {
 
-    private TextField pesquisarTextField = new TextField("input[name=\"q\"]");
-    private Button pesquisaGoogleButton = new Button("#tsf > div:nth-child(2) > div.A7Yvie.emca > div.zGVn2e > button");
+    private TextField pesquisarTextField;
+    private Button pesquisaGoogleButton;
+
+    public HomePage() {
+
+        if ("true".equals(String.valueOf(System.getProperty("browser.mobile")))) {
+
+            pesquisarTextField = new TextField("input[name=\"q\"]");
+            pesquisaGoogleButton = new Button("#tsf > div:nth-child(2) > div.A7Yvie.emca > div.zGVn2e > button");
+
+        } else {
+
+            pesquisarTextField = new TextField("input[name=\"q\"]");
+            pesquisaGoogleButton = new Button("form#tsf div.aajZCb > div > center > input[name=\"btnK\"]");
+
+        }
+
+    }
 
     @Override
     public String getUrl() {
--- src/main/java/page/search/SearchPage.java
+++ src/main/java/page/search/SearchPage.java
@@ -5,7 +5,17 @@ import page.Page;
 
 public class SearchPage extends Page {
 
-    private Text primeiroResultadoText = new Text("#rso > div:nth-child(1) > div:nth-child(1) > div > div > div:nth-child(1) > div > a > div.MUxGbd.v0nnCb");
+    private Text primeiroResultadoText;
+
+    public SearchPage() {
+
+        if ("true".equals(String.valueOf(System.getProperty("browser.mobile")))) {
+            primeiroResultadoText = new Text("#rso > div:nth-child(1) > div:nth-child(1) > div > div > div:nth-child(1) > div > a > div.MUxGbd.v0nnCb");
+        } else {
+            primeiroResultadoText = new Text("#rso > div:nth-child(1) > div > div:nth-child(1) > div > div > div.r > a:nth-child(1) > h3");
+        }
+
+    }
 
     @Override
     public String getUrl() {
